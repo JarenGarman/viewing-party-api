878f4c0387b33efcf26c97bb5b1deb2bcf9897ca
Jaren Garman <jarengarman@protonmail.com>
2025-03-25 17:25:00 -0400

    chore: Add tmdb read access token

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,6 +75,10 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data("<TMDB_API_KEY>") { Rails.application.credentials.tmdb[:key] }
+  config.filter_sensitive_data("<TMDB_READ_ACCESS_TOKEN>") { Rails.application.credentials.tmdb[:token] }
   config.default_cassette_options = {re_record_interval: 30.days}
   config.configure_rspec_metadata!
+  config.before_record do |i|
+    i.response.body.force_encoding("UTF-8")
+  end
 end
