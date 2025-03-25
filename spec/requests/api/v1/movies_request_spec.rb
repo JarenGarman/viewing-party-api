28cc5071c1eddaeeb1e8267ff2639c0d06c05deb
Jaren Garman <jarengarman@protonmail.com>
2025-03-25 18:15:10 -0400

    test: Add test for searching by title

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -16,5 +16,21 @@ RSpec.describe "Movies Endpoint" do
         expect(movie[:attributes][:vote_average]).to be_a(Float)
       end
     end
+
+    it "can search movie titles", :vcr do
+      get "/api/v1/movies?search=Shrek"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data].count).to eq(20)
+      json[:data].each do |movie|
+        expect(movie[:id]).to be_an(Integer)
+        expect(movie[:type]).to eq("movie")
+        expect(movie[:attributes][:title]).to be_a(String)
+        expect(movie[:attributes][:title].downcase).to include("shrek")
+        expect(movie[:attributes][:vote_average]).to be_a(Float)
+      end
+    end
   end
 end
