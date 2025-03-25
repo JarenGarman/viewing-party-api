1247d70b5ce90dc26d7a6116fa3bf11e9c6c50bb
Jaren Garman <jarengarman@protonmail.com>
2025-03-25 16:58:43 -0400

    test: Add test for top rated movies

---
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "Movies Endpoint" do
+  describe "happy path" do
+    it "can retrieve top rated movies", :vcr do
+      get "/api/v1/movies"
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data].count).to eq(20)
+      json[:data].each do |movie|
+        expect(movie[:id]).to be_an(Integer)
+        expect(movie[:type]).to eq("movie")
+        expect(movie[:attributes][:title]).to be_a(String)
+        expect(movie[:attributes][:vote_average]).to be_a(Float)
+      end
+    end
+  end
+end
