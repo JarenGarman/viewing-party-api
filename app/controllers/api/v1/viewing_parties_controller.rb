170a413d0ea1ec484aa48024263ea70931925a79
Jaren Garman <jarengarman@protonmail.com>
2025-03-26 18:02:57 -0400

    feat: Add viewing party controller and serializer

---
+++ app/controllers/api/v1/viewing_parties_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::ViewingPartiesController < ApplicationController
+  def create
+    if !User.find(params[:user_id])
+      render json: ErrorSerializer.format_error(ErrorMessage.new("Host user does not exist", 404)), status: :not_found
+    end
+    viewing_party = ViewingParty.new(viewing_party_params)
+    if viewing_party.save
+      params[:invitees].each do |user_id|
+        viewing_party.viewing_party_users.create(user_id: user_id)
+      end
+      render json: ViewingPartySerializer.new(viewing_party), status: :created
+    else
+      render json: ErrorSerializer.format_error(ErrorMessage.new(viewing_party.errors.full_messages.to_sentence, 400)), status: :bad_request
+    end
+  end
+
+  private
+
+  def viewing_party_params
+    params.permit(:name, :start_time, :end_time, :movie_id, :movie_title, :invitees, :user_id)
+  end
+end
