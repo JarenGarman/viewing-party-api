b07587ff0e6cb4fa14f784cf8f73ac959c313a78
Jaren Garman <jarengarman@protonmail.com>
2025-03-26 15:56:28 -0400

    test: add test to attempt adding nonexistent invitee

--- spec/requests/api/v1/user/viewing_party_request_spec.rb
+++ spec/requests/api/v1/user/viewing_party_request_spec.rb
@@ -38,6 +38,29 @@ RSpec.describe "Viewing Party API", type: :request do
           expect(user[:type]).to eq("user")
         end
       end
+
+      it "returns 201 Created and provides expected fields with nonexistent invitee" do
+        party_params[:invitees] << 1000000
+
+        post api_v1_user_viewing_party_path(user3.id), params: party_params, as: :json
+
+        expect(response).to have_http_status(:created)
+        json = JSON.parse(response.body, symbolize_names: true)
+        expect(json[:data][:type]).to eq("viewing_party")
+        expect(json[:data][:id]).to eq(ViewingParty.last.id.to_s)
+        expect(json[:data][:attributes]).to include(
+          name: "Juliet's Bday Movie Bash!",
+          start_time: "2025-02-01 10:00:00",
+          end_time: "2025-02-01 14:30:00",
+          movie_id: 278,
+          movie_title: "The Shawshank Redemption"
+        )
+        expect(json[:relationships][:users][:data].count).to eq(3)
+        json[:relationships][:users][:data].each do |user|
+          expect(user[:id]).to eq(user1.id.to_s).or eq(user2.id.to_s).or eq(user4.id.to_s)
+          expect(user[:type]).to eq("user")
+        end
+      end
     end
 
     context "with invalid request" do
