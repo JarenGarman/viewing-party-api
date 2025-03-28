cf632613495622c94799cfbd51e335ec9bc7fe99
Jaren Garman <jarengarman@protonmail.com>
2025-03-28 17:45:19 -0400

    chore: Rename response to movies because it makes more sense

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    response = if params[:search]
+    movies = if params[:search]
       MovieGateway.search_movies(params[:search])
     else
       MovieGateway.top_rated_movies
     end
 
-    render json: MovieSerializer.new(response)
+    render json: MovieSerializer.new(movies)
   end
 end
