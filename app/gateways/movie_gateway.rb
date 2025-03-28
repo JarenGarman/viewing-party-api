c8e1345daeda9af58c606b7a38e2e92f27a9e8e8
Jaren Garman <jarengarman@protonmail.com>
2025-03-28 17:41:23 -0400

    feat: Add top_rated_movies and search_movies to movie gateway

--- app/gateways/movie_gateway.rb
+++ app/gateways/movie_gateway.rb
@@ -6,6 +6,26 @@ class MovieGateway
     Movie.new(json)
   end
 
+  def self.top_rated_movies
+    response = connection.get("/3/movie/top_rated")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:results].first(20).map do |data|
+      Movie.new(data)
+    end
+  end
+
+  def self.search_movies(search)
+    response = connection.get("/3/search/movie") do |req|
+      req.params[:query] = search.downcase
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:results].first(20).map do |data|
+      Movie.new(data)
+    end
+  end
+
   private_class_method
 
   def self.connection
