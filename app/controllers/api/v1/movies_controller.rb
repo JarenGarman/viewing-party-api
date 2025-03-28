523b9a6d8997dd3ee0060f1cd696e0c144a8cf06
Jaren Garman <jarengarman@protonmail.com>
2025-03-28 17:43:49 -0400

    refactor: Movies controller and serializer use new movie poro and movie gateway

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,19 +1,11 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://api.themoviedb.org") do |faraday|
-      faraday.request :authorization, "Bearer", Rails.application.credentials.tmdb[:token]
-    end
-
     response = if params[:search]
-      conn.get("/3/search/movie") do |req|
-        req.params[:query] = params[:search].downcase
-      end
+      MovieGateway.search_movies(params[:search])
     else
-      conn.get("/3/movie/top_rated")
+      MovieGateway.top_rated_movies
     end
 
-    json = JSON.parse(response.body, symbolize_names: true)
-
-    render json: MovieSerializer.new(json[:results].first(20))
+    render json: MovieSerializer.new(response)
   end
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,16 +1,4 @@
 class MovieSerializer
   include JSONAPI::Serializer
-
-  set_type :movie
-  set_id do |movie|
-    movie[:id]
-  end
-
-  attribute :title do |movie|
-    movie[:title]
-  end
-
-  attribute :vote_average do |movie|
-    movie[:vote_average]
-  end
+  attributes :title, :vote_average
 end
