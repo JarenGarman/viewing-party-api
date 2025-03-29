f0fd7ca5faab6e671db746895c02a5b66d04df03
Jaren Garman <jarengarman@protonmail.com>
2025-03-29 14:01:48 -0400

    feat: Add new attributes and instance methods for movie poro

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,13 +1,31 @@
 class Movie
   attr_reader :id,
     :title,
+    :release_year,
     :vote_average,
-    :runtime
+    :runtime,
+    :genres,
+    :summary,
+    :cast,
+    :total_reviews,
+    :reviews
 
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
+    @release_year = data[:release_year]
     @vote_average = data[:vote_average]
     @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:overview]
+  end
+
+  def add_reviews(data)
+    @reviews = data[:results].first(5)
+    @total_reviews = data[:total_results]
+  end
+
+  def add_cast(data)
+    @cast = data[:cast].first(10)
   end
 end
