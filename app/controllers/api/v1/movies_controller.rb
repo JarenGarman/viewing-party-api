7e1ce6cfcecafd881bd36f4ec648aa581bb379e9
Jaren Garman <jarengarman@protonmail.com>
2025-03-29 15:08:41 -0400

    feat: Add show action for movie details

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::MoviesController < ApplicationController
+  def show
+    movie = MovieGateway.movie_details(params[:id])
+
+    render json: MovieSerializer.new(movie)
+  end
+
   def index
     movies = if params[:search]
       MovieGateway.search_movies(params[:search])
