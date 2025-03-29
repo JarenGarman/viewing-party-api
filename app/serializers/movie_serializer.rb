22b0fa3496f31f0185c234ae3dfdfcea332bf5cb
Jaren Garman <jarengarman@protonmail.com>
2025-03-29 14:45:42 -0400

    feat: Add new movie attributes to movie serailizer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,35 @@
 class MovieSerializer
   include JSONAPI::Serializer
-  attributes :title, :vote_average
+  attributes :title,
+    :release_year,
+    :vote_average,
+    :summary,
+    :total_reviews
+
+  attribute :runtime do |movie|
+    h, m = movie.runtime.divmod(60)
+    "#{h} hours, #{m} minutes"
+  end
+
+  attribute :genres do |movie|
+    movie.genres.pluck(:name)
+  end
+
+  attribute :cast do |movie|
+    movie.cast.map do |cast_member|
+      {
+        character: cast_member[:character],
+        actor: cast_member[:name]
+      }
+    end
+  end
+
+  attribute :reviews do |movie|
+    movie.reviews.map do |review|
+      {
+        author: review[:author],
+        review: review[:content]
+      }
+    end
+  end
 end
